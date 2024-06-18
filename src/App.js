e03c1fd352ba33376d6e26cae900d2b854d581fe
Dinis <franciscodins806@gmail.com>
2024-06-18 12:02:17 +0100

    ze lindo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import ProductList from "./paginas/Produtos";
+import Login from "./paginas/login";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Produtos from "./paginas/Produtos";
+import Detalhes from "./paginas/detalhes";
+import Navbar from "./components/Navbar/navbar";
+import Registo from "./paginas/registo";
+import Novo from "./paginas/novoproduto";
+import Carrinho from "./paginas/carrinho";
+import Rodape from "./components/rodape/rodape"
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <Router>
+      <Navbar/>
+       <Routes>
+          
+          <Route path="/" element= {<ProductList/>}/>
+          <Route path="/login" element= {<Login/>}/>
+          <Route path="/produtos" element= {<Produtos/>}/>
+          <Route path="/produtos/:id" element= {<Detalhes/>}/>
+          <Route path="/registo" element= {<Registo/>}/>
+          <Route path="/novoproduto" element= {<Novo/>}/>
+          <Route path="/carrinho" element= {< Carrinho/>}/>
+      </Routes>
+      <Rodape/>
+    </Router>
 
+  );
+};
 export default App;
+ 
